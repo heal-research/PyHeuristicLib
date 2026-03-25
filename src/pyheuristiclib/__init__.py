c5e90341dd979d83854d842440e6992388543970
Bernhard Werth <bernhard.werth@fh-hagenberg.at>
2026-03-25 14:02:46 +0100

    add quotes

--- src/pyheuristiclib/__init__.py
+++ src/pyheuristiclib/__init__.py
@@ -12,7 +12,7 @@ def load_dotnet_dll():
     dll_name = "HEAL.HeuristicLib.Core.dll"
 
     # Use importlib.resources to find the DLL inside the package
-    with pkg_resources.path(pyheuristiclib, dll_name) as dll_path:
+    with pkg_resources.path("pyheuristiclib", dll_name) as dll_path:
         abs_path = str(dll_path.resolve())
         System.Reflection.Assembly.LoadFrom(abs_path)
         clr.AddReference(dll_name.split(".")[0])
